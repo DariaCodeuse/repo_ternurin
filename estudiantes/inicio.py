3fb36c3ee30c055ac70b5efddb0446d056a1cb78
Karla Díaz <diazaguilarkarla8@gmail.com>
2025-04-28 01:44:46 -0600

    docs: functionalities add in to main module

--- estudiantes/inicio.py
+++ estudiantes/inicio.py
@@ -1,9 +1,20 @@
 import streamlit as st
+from db import get_db
+from models import Contenido
+from utils import buscador_contenido, mostrar_contenido_reciente, panel_materias
 
 def main():
     st.title("🏠 Inicio")
     st.write("Bienvenido al Repositorio Ternurin!")
     st.info("Explora los contenidos disponibles por materia.")
+
+    st.image("./images/repositorio.png", use_container_width=True)
+    db = next(get_db())
+
+    # Secciones
+    buscador_contenido(db, Contenido)
+    mostrar_contenido_reciente(db, Contenido)
+    panel_materias()
 
 if __name__ == "__main__":
     main()
